49f9239ef22a7d80a5eff0f868f4d04a0763ac01
Vaibhav Chauhan <vbv_chn7@outlook.com>
2021-02-13 17:52:43 +0530

    UnitTest - Deleted 'Should Render Title' Test Case because title is rendered from title service and not from - div with class content including span in it

--- Code/Frontend/src/app/app.component.spec.ts
+++ Code/Frontend/src/app/app.component.spec.ts
@@ -36,11 +36,4 @@ describe('AppComponent', () => {
         const app = fixture.componentInstance;
         expect(app.title).toEqual('webStore');
     });
-
-    xit('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain('webStore app is running!');
-    });
 });
